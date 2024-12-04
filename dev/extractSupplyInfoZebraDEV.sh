9a716776f2b7b4625bfe0c465a429a907f0acfe9
dismad <81990132+dismad@users.noreply.github.com>
2024-12-03 19:47:42 -0800

    Create extractSupplyInfoZebraDEV.sh
    * DEV version of extractSupplyInfo
    * Zebra support
    * Lockbox support
    * Uses custom ToCurl script so zcash-cli binary not needed

---
+++ dev/extractSupplyInfoZebraDEV.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+b=($(./toCurl.sh getblockchaininfo | jq . |  grep 'chainValue": [0-9]+' -Eo)) 
+b=("${b[@]#chainValue}")
+
+SSupply=$(echo "scale=0; ${b[3]} + ${b[5]} + ${b[7]} + ${b[9]}" | bc)
+TSupply=$(echo "scale=0; ${b[1]} + $SSupply" | bc)
+
+echo
+echo "Total Chain Supply       : $TSupply"
+
+echo "Total Transparent supply : ${b[1]}"
+
+echo "Total Sprout supply      : ${b[3]}"
+
+echo "Total Sapling supply     : ${b[5]}"
+
+echo "Total Orchard supply     : ${b[7]}"
+
+echo "Total Deferred supply    : ${b[9]}"
+
+echo "-----------------------------------"
+echo "Total Shielded Supply    : $SSupply"
