27ba8539f667b68b73a4ddae1af31fa2a53fcb85
dismad <81990132+dismad@users.noreply.github.com>
2024-12-07 12:04:23 -0800

    Update listTXs.sh
    toCurl support

--- listTXs.sh
+++ listTXs.sh
@@ -4,5 +4,5 @@ startBlock="${1}"   #1 represent 1st argument
 endBlock="${2}"     #2 represent 2st argument
 
 
-for i in `seq $startBlock $endBlock`; do zcash-cli getblock $i | jq .tx.[]; done
+for i in `seq $startBlock $endBlock`; do ./toCurl.sh getblock $i | jq .result.tx.[]; done
 
