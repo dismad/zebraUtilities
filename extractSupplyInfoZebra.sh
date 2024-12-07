45ab4c10563cc92fc0317ce0e3f5a52e3160fbab
dismad <81990132+dismad@users.noreply.github.com>
2024-12-07 12:13:07 -0800

    Update extractSupplyInfoZebra.sh
    toCurl support, lockbox support

--- extractSupplyInfoZebra.sh
+++ extractSupplyInfoZebra.sh
@@ -1,22 +1,26 @@
 #!/bin/bash
 
-a=($(zcash-cli getblockchaininfo | jq .valuePools.[].chainValue))
+b=($(./toCurl.sh getblockchaininfo | jq . |  grep 'chainValue": [0-9]+' -Eo)) 
+b=("${b[@]#chainValue}")
 
-SSupply=$(echo "${a[1]} + ${a[2]} + ${a[3]}" | bc)
-TSupply=$(echo "${a[0]} + $SSupply" | bc)
+SSupply=$(echo "scale=0; ${b[3]} + ${b[5]} + ${b[7]} + ${b[9]}" | bc)
+TSupply=$(echo "scale=0; ${b[1]} + $SSupply" | bc)
 
 echo
 echo "Total Chain Supply       : $TSupply"
 
-echo "Total Transparent supply : ${a[0]}"
+echo "Total Transparent supply : ${b[1]}"
 
-echo "Total Sprout supply      : ${a[1]}"
+echo "Total Sprout supply      : ${b[3]}"
 
-echo "Total Sapling supply     : ${a[2]}"
+echo "Total Sapling supply     : ${b[5]}"
 
-echo "Total Orchard supply     : ${a[3]}"
+echo "Total Orchard supply     : ${b[7]}"
+
+echo "Total Lockbox supply    : ${b[9]}"
 
 echo "-----------------------------------"
 echo "Total Shielded Supply    : $SSupply"
+
 
 
