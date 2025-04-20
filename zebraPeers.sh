fe29617822594284f70556620d351a17c17fbc55
dismad <81990132+dismad@users.noreply.github.com>
2025-04-20 14:29:36 -0700

    Update zebraPeers.sh
    bug

--- zebraPeers.sh
+++ zebraPeers.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-peers=$(./toCurl.sh getpeerinfo | jq .result[].addr | tr -d '"' | wc -l)
+peers=$(./toCurl.sh getpeerinfo | jq .[].addr | tr -d '"' | wc -l)
 
 echo
 echo "-------------------------------------------"
-echo "$(./toCurl.sh getpeerinfo | jq .result[].addr | tr -d '"')"
+echo "$(./toCurl.sh getpeerinfo | jq .[].addr | tr -d '"')"
 echo "-------------------------------------------"
 echo
 echo "Your Zebrad node has $peers ip's connected."
