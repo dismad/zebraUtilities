a89ac772cf45773aa275c9af13ce8d3e70216658
dismad <81990132+dismad@users.noreply.github.com>
2025-04-21 12:27:07 -0700

    Update getDateFromTX.sh
    bug

--- getDateFromTX.sh
+++ getDateFromTX.sh
@@ -1,16 +1,5 @@
-#!/bin/bash
-
+ #!/bin/bash
+ 
 txID="${1}"   #1 represent 1st argument
 
-rawTx=$(./toCurl.sh getrawtransaction $txID 1 | jq .)
-isZebra=$(./toCurl.sh getinfo | jq .subversion | grep -o Zebra)
-result=""
-
-if [ "$isZebra" = "Zebra" ];then
-        myBlock=$(echo "$rawTx" | jq .height)
-        result=$(./toCurl.sh getblock $myBlock | jq .time | xargs -i date -d @"{}")
-        echo "$result"
-else
-        ./txDetailsZebra.sh $txID | jq .time | xargs -i date -d @"{}"
-fi
-
+./toCurl.sh getrawtransaction $txID 1 | jq .height | xargs -n1 ./toCurl.sh getblock | jq .time | xargs -i date -d @"{}"
