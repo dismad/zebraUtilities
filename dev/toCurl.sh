f665d0ba7041b90c79b86f7a188386735dea4519
dismad <81990132+dismad@users.noreply.github.com>
2025-05-06 11:28:32 -0700

    Update toCurl.sh
    rawmempool supported added

--- dev/toCurl.sh
+++ dev/toCurl.sh
@@ -14,6 +14,8 @@ credentials="$user:$pw"
 
 if [ "$command" == "getmetrics" ]; then
         myCurl="curl -s --data-binary '{\"jsonrpc\": \"2.0\", \"id\":0, \"method\": \"$command\", \"params\": [] }' -H 'content-type: application/json' http://127.0.0.1:$port"
+elif [ "$command" == "getrawmempool" ]; then
+        myCurl="curl -s -u $credentials --data-binary '{\"jsonrpc\": \"1.0\", \"id\":\"curltest\", \"method\": \"$command\", \"params\": [$arg1] }' -H 'content-type: application/json' http://127.0.0.1:$port/"
 elif [ -n "$arg1" ]; then
         if [ -n "$arg2" ]; then
                 myCurl="curl -s -u $credentials --data-binary '{\"jsonrpc\": \"1.0\", \"id\":\"curltest\", \"method\": \"$command\", \"params\": [\"$arg1\", $arg2] }' -H 'content-type: application/json' http://127.0.0.1:$port/"
